4585fb7e4aae3e84b4f6e6262c4a486e91bdca20
石泉 慎一 <rrmkk086@yahoo.co.jp>
2020-03-14 20:56:40 +0900

    2020/3/14 PlcSendClass

---
+++ PlcSendClass.pas
@@ -0,0 +1,18 @@
+//PLCSEnd—p‚ÌƒNƒ‰ƒX
+
+unit PlcSendClass;
+
+interface
+  type
+     TPlcSend = class(TObject)
+     public
+     private
+  end;
+
+
+var
+  PlcSend : TPlcSend;
+
+implementation
+
+end.
